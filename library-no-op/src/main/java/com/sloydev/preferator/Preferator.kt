f5dab922f34e0b5176df820e140ead976bd92fe6
Sergi Martínez <sergi.martinez@gmail.com>
2020-08-14 12:24:49 +0200

    Added @JvmStatic to allow invocation from Java

--- library-no-op/src/main/java/com/sloydev/preferator/Preferator.kt
+++ library-no-op/src/main/java/com/sloydev/preferator/Preferator.kt
@@ -3,6 +3,7 @@ package com.sloydev.preferator
 import android.content.Context
 
 object Preferator {
+  @JvmStatic
   fun launch(context: Context?) {
     // no-op
   }
--- library/src/main/java/com/sloydev/preferator/Preferator.kt
+++ library/src/main/java/com/sloydev/preferator/Preferator.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 
 object Preferator {
+  @JvmStatic
   fun launch(context: Context) {
     context.startActivity(Intent(context, PreferatorActivity::class.java))
   }
